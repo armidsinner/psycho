e30b73295560ce772cb3eb224434973a385ace38
hxnbsn <zaft.margaret@gmail.com>
2022-05-17 00:53:28 +0300

    ya ebal

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,6 @@
-class PersonsController < ApplicationController
+class CommentsController < ApplicationController
   def create
-    @post = Post.find(params[:id])
+    @post = Post.find(params[:post_id])
     @comment = @post.comments.new(comment_params)
     @comment.user = current_user
     if @comment.save
